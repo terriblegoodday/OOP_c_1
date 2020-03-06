15a9c41153620e4c6d6909975c4feac9a58e3816
Eduard <terriblegoodday@icloud.com>
2020-03-07 09:30:56 +1000

    Initial Commit

---
+++ OOP_c_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOP_c_1
+//
+//  Created by Eduard Dzhumagaliev on 3/7/20.
+//  Copyright © 2020 Eduard Dzhumagaliev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
